7e5bd093f703aef24339b5ba80b510579674fdd0
Zack Guard <zacharyguard@gmail.com>
2015-12-06 00:00:27 +0800

    Add timeline and content

---
+++ js/basic.js
@@ -0,0 +1,30 @@
+/* timeline */
+
+$(".timeline").each(function(i, elem) {
+    var $timelineElem = $(elem);
+
+    $timelineElem
+        .append($("<div class='timeline_dates timeline_dates--left'></div>"))
+        .append($("<div class='timeline_axis'>"))
+        .append($("<div class='timeline_dates timeline_dates--right'></div>"));
+
+    $timelineElem.find("ol").each(function(i, olElem) {
+        var $olElem = $(olElem);
+
+        var side = ["left", "right"][i % 2];
+
+        moment.locale("en-GB");
+
+        var dateFormatted = moment(olElem.dataset.date).format("MMMM Do YYYY");
+        var content = $olElem.html();
+
+        $dateElem = $("<ol class='timeline_date'><h6>" + dateFormatted + "</h6><p>" + content + "</p></ol>");
+        if (olElem.classList.contains("timeline_date--highlight")) {
+            $dateElem.addClass("timeline_date--highlight");
+        }
+
+        $timelineElem.find(".timeline_dates--" + side).append($dateElem);
+
+        $olElem.remove();
+    });
+});
