030bcac9ced5e1411fcca9a8d1204a9d2ca8ae62
Zack Guard <zacharyguard@gmail.com>
2015-12-15 21:53:49 +0800

    Add register form

--- js/basic.js
+++ js/basic.js
@@ -26,3 +26,27 @@ $(".timeline").each(function(i, elem) {
         $olElem.remove();
     });
 });
+
+/* form */
+
+$("form").on("change", function(e) {
+    var $form = $(this);
+
+    $form.find("[data-form-showif]").each(function(i, elem) {
+        var $elem = $(elem);
+
+        var cond = $elem.attr("data-form-showif");
+        var condArr = cond.split("=");
+
+        if (condArr.length === 2) {
+            var condField = $form.find("[data-form-field='" + condArr[0] + "']");
+            var condValue = condArr[1];
+
+            if (condField.val() === condValue) {
+                $elem.removeClass("hidden");
+            } else {
+                $elem.addClass("hidden");
+            }
+        }
+    });
+});
